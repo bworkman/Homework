66485e8c69f64010a44cd87f6a9672b4a3d401a8
bworkman <brandtworkman@hotmail.com>
2015-05-18 20:25:19 -0600

    added validations

--- address_book/app/models/contact.rb
+++ address_book/app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, length: { is: 10 }
+  validates :phone, presence: true
 end
